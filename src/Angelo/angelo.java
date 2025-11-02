31f279d872bbad4672e15ac9a3609adbe1af7bf8
Angelo <maanflodi@gmail.com>
2025-11-01 22:28:24 -0500

    prueba

--- src/Angelo/angelo.java
+++
@@ -1,5 +0,0 @@
-package Angelo;
-
-public class angelo {
-
-}
---
+++ src/Interfaz/Arranque/StarSystem.java
@@ -0,0 +1,9 @@
+package Interfaz.Arranque;
+
+import javax.swing.*;
+
+public class StarSystem extends JFrame {
+    public StarSystem(){
+
+    }
+}
